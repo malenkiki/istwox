003685841e3448c9bf0e1d36b4506735ae55eb3c
Michel Petit <petit.michel@gmail.com>
2011-03-15 01:31:00 +0100

    Add method to compute check digit for ISAN

--- lib/istwox/isan.rb
+++ lib/istwox/isan.rb
@@ -1,16 +1,55 @@
 module Istwox
     class ISAN
+        attr_accessor :original, :root, :episode, :version, :check_digit_isan
+
         def initialize(original)
-
+            @original = original
+            # quick add, to check the good calcul of check digit, this must
+            # change
+            @check_digit_isan = compute_check_digit(original[0, 16])
         end
 
-        def root 
+        def compute_check_digit(str)
+            s = as = p = ap = 0
+            mod_one = 37
+            mod_two = mod_one - 1
+
+            str.downcase.chars.map.with_index do |c, i|
+                if c[/\d+/]
+                    s = c[/\d+/].to_i
+                else
+                    s = c.ord - 87
+                end
+
+                s = s + mod_two if i.zero?
+                s = s + ap      if i > 0
+
+                as = s
+                as = s - mod_two if s > mod_two
+
+                as = mod_two if as.zero?
+
+                p = as * 2
+
+                ap = p
+                ap = p - mod_one if p >= mod_one
+            end
+
+            return '0'                     if ap == 1
+            return (mod_one - ap).to_s     if (mod_one - ap < 10)
+            return (mod_one - ap + 55).chr if (mod_one - ap >= 10)
         end
 
-        def episode
-        end
+    end
 
-        def version
+    class VISAN < ISAN
+        attr_accessor :check_digit_visan
+
+        def initialize(original)
+            @original = original
+            # quick add, to check the good calcul of check digit, this must
+            # change
+            @check_digit_visan = compute_check_digit(original)
         end
     end
 end
--- spec/isan_spec.rb
+++ spec/isan_spec.rb
@@ -5,7 +5,6 @@ require File.expand_path(File.join('.', 'spec_helper'), File.dirname(__FILE__))
 # Stargate Atlantis - Season 1 - Disc 2: 0000-0000-DDB0-0069-Q-0000-0000-X
 # Stargate Atlantis - Season 1 - Disc 3: 0000-0000-DDB0-006A-O-0000-0000-2
 # Stargate Atlantis - Season 1 - Disc 4: 0000-0000-DDB0-006B-M-0000-0000-8
-# see http://www.finseth.com/isan.html
 describe Istwox::ISAN, "Test International Standard Audiovisual Number" do
 
   it "'0000-0002-B3C8-0000-3-0000-0000-S' should create valid ISAN" do
@@ -33,15 +32,13 @@ describe Istwox::ISAN, "Test International Standard Audiovisual Number" do
   end
 
   it "'0000-0002-B3C8-0000-3-0000-0000-S' should have first check digit '3'" do
-      pending "ISAN must be implement first"
-      ismn = Istwox::ISAN.new "0000-0002-B3C8-0000-3-0000-0000-S"
-      ismn.first_check_digit.should eq('3')
+      isan = Istwox::ISAN.new "00000002B3C80000300000000S"
+      isan.check_digit_isan.should eq('3')
   end
 
   it "'0000-0002-B3C8-0000-3-0000-0000-S' should have second check digit 'S'" do
-      pending "ISAN must be implement first"
-      ismn = Istwox::ISAN.new "0000-0002-B3C8-0000-3-0000-0000-S"
-      ismn.second_check_digit.should eq('S')
+      visan = Istwox::VISAN.new "00000002B3C8000000000000"
+      visan.check_digit_visan.should eq('S')
   end
 end
 
