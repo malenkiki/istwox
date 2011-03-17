687934cf79df865c26ed4f834e0c685bcef59a9e
Michel Petit <petit.michel@gmail.com>
2011-03-17 22:49:38 +0100

    Write great part of class ISTC and its tests

--- lib/istwox/istc.rb
+++ lib/istwox/istc.rb
@@ -1,35 +1,63 @@
 module Istwox
     class ISTC
-        attr_accessor :original, :code, :weight, :check_digit
+        attr_reader :original, :code, :weight, :check_digit
 
+        # todo: put is_valid to raise exception in some cases…
         def initialize(original)
             @original = original
+            @code = []
+            extract_code
+            create_weight_index
+            calculate_check_digit
+        end
+
+
+        def registration
+            @code[0, 3].join
+        end
+
+        def year
+            @code[3, 4].join
+        end
+
+        def textual_work
+            @code[7, 8].join
+        end
+
+        def to_s
+            'ISTC ' + registration() + '-' + year() + '-' + textual_work() + '-' + @check_digit
+        end
+        
+        private
+        def create_weight_index
             @weight = [11, 9, 3, 1] * 4
             @weight.delete_at 15
         end
 
+        def calculate_weight_sum
+            sum = 0
+
+            @code.take(15).map.with_index do |v, k|
+                sum += v.to_i(16) * @weight[k]
+            end
+
+            sum
+        end
+
+        # todo
         def is_valid?
         end
 
         def extract_code
             @original.chars.each do |c|
+                if [('0'..'9').to_a,('A'..'F').to_a].flatten!.include? c.upcase
+                    @code.push c.upcase
+                end
             end
         end
 
-
         def calculate_check_digit
-        end
-
-        def registration
-        end
-
-        def year
-        end
-
-        def textual_work
-        end
-
-        def to_s
+            @check_digit = (calculate_weight_sum % 16).to_s(16).upcase
         end
     end
 end
--- spec/istc_spec.rb
+++ spec/istc_spec.rb
@@ -1,12 +1,82 @@
 # -*- coding: utf-8 -*-
 require File.expand_path(File.join('.', 'spec_helper'), File.dirname(__FILE__))
 
-describe Istwox::ISTC, "Test  implementation" do
+describe Istwox::ISTC, "Test International Standard Text Code implementation" do
 
   it "" do
-      pending "Must write tests for ISTC"
+      pending "Must more tests for ISTC"
       istc = Istwox::ISTC.new ""
       istc.should be
+  end
+
+  it "03A-2009-000C299F-D should have check digit 'D'" do
+      istc = Istwox::ISTC.new "03A-2009-000C299F-D"
+      istc.check_digit.should == 'D'
+  end
+  
+  it "0A3-2009-012445C9-B should have check digit 'B'" do
+      istc = Istwox::ISTC.new "0A3-2009-012445C9-B"
+      istc.check_digit.should == 'B'
+  end
+  
+  it "03A2009000C299F should have check digit 'D'" do
+      istc = Istwox::ISTC.new "03A2009000C299F"
+      istc.check_digit.should == 'D'
+  end
+  
+  it "0A32009012445C9 should have check digit 'B'" do
+      istc = Istwox::ISTC.new "0A32009012445C9"
+      istc.check_digit.should == 'B'
+  end
+  
+  it "03A2009000C299F should have registration element '03A'" do
+      istc = Istwox::ISTC.new "03A2009000C299F"
+      istc.registration.should == '03A'
+  end
+  
+  it "0A32009012445C9 should have registration element '0A3'" do
+      istc = Istwox::ISTC.new "0A32009012445C9"
+      istc.registration.should == '0A3'
+  end
+  
+  it "03A2009000C299F should have year element '2009'" do
+      istc = Istwox::ISTC.new "03A2009000C299F"
+      istc.year.should == '2009'
+  end
+  
+  it "0A32009012445C9 should have year element '2009'" do
+      istc = Istwox::ISTC.new "0A32009012445C9"
+      istc.year.should == '2009'
+  end
+
+  it "0A32009012445C9 should have textual work element '012445C9'" do
+      istc = Istwox::ISTC.new "0A32009012445C9"
+      istc.textual_work.should == '012445C9'
+  end
+  
+  it "03A2009000C299F should have textual work element '000C299F'" do
+      istc = Istwox::ISTC.new "03A2009000C299F"
+      istc.textual_work.should == '000C299F'
+  end
+  
+  it "03A-2009-000C299F-D should be formated to 'ISTC 03A-2009-000C299F-D'" do
+      istc = Istwox::ISTC.new "03A-2009-000C299F-D"
+      istc.to_s.should == 'ISTC 03A-2009-000C299F-D'
+  end
+  
+  it "0A3-2009-012445C9-B should be formated to 'ISTC 0A3-2009-012445C9-B'" do
+      istc = Istwox::ISTC.new "0A3-2009-012445C9-B"
+      istc.to_s.should == 'ISTC 0A3-2009-012445C9-B'
+  end
+  
+  it "03A2009000C299F should be formated to 'ISTC 03A-2009-000C299F-D'" do
+      istc = Istwox::ISTC.new "03A2009000C299F"
+      istc.to_s.should == 'ISTC 03A-2009-000C299F-D'
+  end
+  
+  it "0A32009012445C9 should be formated to 'ISTC 0A3-2009-012445C9-B'" do
+      istc = Istwox::ISTC.new "0A32009012445C9"
+      istc.to_s.should == 'ISTC 0A3-2009-012445C9-B'
   end
   
 end
